9697f18e668476b326e6ab08eaa7df4fe5f57508
nghodsi <navid@ghodsi.us>
2020-11-18 00:49:50 -0800

    Added game button

--- public/main.js
+++ public/main.js
@@ -2,3 +2,25 @@ function changeText()
 {
  document.getElementById("heading").innerHTML = "Who's Joe?";
 }
+
+
+function loadcontent(content) {
+  alert (content)
+$("#game_content").html(content)
+
+}
+
+$(document).ready(function () {
+  var playButton = $("#play_button")
+  playButton.click(function(){
+    alert("button")
+ $.ajax({
+   url: "game.html",
+   crossDomain: true,
+   success:loadcontent
+ })
+
+
+  })
+
+});
